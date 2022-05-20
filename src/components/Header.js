e0ae025d96183c9672b19a8a228aa4e86ee2fd1a
troymeeker <troymeeker1@gmail.com>
2022-05-19 21:36:24 -0700

    current time added in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 function Header(){
+  
+    const date = new Date().toLocaleDateString()
+    const time = new Date().toLocaleTimeString()
+    const hour = new Date().getHours()
+
+    
     return(
         <div className='header'>
-            <h3> Good Afternoon</h3>
+            {/* <h3>Good Evening</h3> */}
+            {hour >= 12 ? hour >= 16 ? <h3>Good Evening</h3> : <h3>Good Afternoon</h3> : <h3>Good Morning</h3>}
+            <h3>Today is {date}</h3>
+            <h3>The current time is {time}</h3>
+            
+             
+
             
         </div>
     )
