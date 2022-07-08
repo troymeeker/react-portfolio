d8a37eeef562dd6676bfe621172ba5331c5dddc4
troymeeker <troymeeker1@gmail.com>
2022-07-07 19:05:48 -0700

    tweak to project formatting

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,7 +55,7 @@ function Main(){
                         <h4>Craigslist clone</h4>
                         <p>Features:  CRUD, React Router, User authentication & authorization </p>
   
-                        <a href="https://mighty-taiga-60616.herokuapp.com/" target='_blank' rel="noreferrer" className="link">Deployed App</a>
+                        <a href="https://mighty-taiga-60616.herokuapp.com/" target='_blank' rel="noreferrer" className="link">Deployed App</a><br/>
                         <iframe width="450" height="250" src="https://www.youtube.com/embed/4GTfhJEG_ig" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
                        
                         <a href="https://github.com/troymeeker/phase-4-rails-project" target='_blank' rel="noreferrer" className="link">code</a>
