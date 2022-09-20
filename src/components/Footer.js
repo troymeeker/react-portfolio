22e05bd777bacdbc87708b4ddf8943b20de5b53b
troymeeker <troymeeker1@gmail.com>
2022-09-19 17:35:45 -0700

    links margin changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ function Footer(){
     return (
         <div className='footer'>
             <div className='footer-grid'>
-                <div className='grid-item' >
+                <div className='grid-item-footer' >
                    <a href='https://www.linkedin.com/in/troy-meeker/' target='_blank' rel="noreferrer" className='footer-item'>linkedin</a> 
                 </div>
                
-                <div className='grid-item'>
+                <div className='grid-item-footer'>
                     <a href='https://github.com/troymeeker' target='_blank' rel="noreferrer" className='footer-item'>github</a> 
                 </div>
-                <div className='grid-item'>
+                <div className='grid-item-footer'>
                    <a href={resume} target='_blank' rel="noreferrer" className='footer-item' >resume</a>
                 </div>
 
